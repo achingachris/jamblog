a1c6d8cc77f5b112045f5992ab22490d6310ca6d
macbook <achinga.chris@gmail.com>
2022-02-14 00:29:00 +0300

    page-layouts

---
+++ layout/Layout.js
@@ -0,0 +1,14 @@
+import NavigationBar from "./NavigationBar"
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <NavigationBar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
